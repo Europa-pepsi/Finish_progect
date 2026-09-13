6c5114d0aab3f5d3b92b5ad96346ad7be5e3f1a5
Gomfic <gricaenkoroma7@gmail.com>
2026-09-13 18:44:11 +0300

    All_base

--- somsing/src/App.jsx
+++ somsing/src/App.jsx
@@ -10,6 +10,15 @@ function App() {
   return (
     <>
       
+
+          <header>
+            <h1 className='name_shop'>Somsing</h1>
+            <h2 className='about_shop'>Online store Somsing.ua</h2>
+          </header>
+          <main>
+            <img className='main_photo' src="https://images.unsplash.com/photo-1751301586011-256d527bc8aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGFzc2V0dG8lMjBjb3JzYXxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+          </main>
+       
     </>
   )
 }
